b0f0cb46c4587319ee0d47ed7df7262710ea4c44
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2022-04-15 18:15:10 -0300

    feat(purchases): método para criar produtos

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,5 +1,8 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ProductsService } from '../../../services/products.service';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { CreateProductInput } from '../inputs/create-product-input';
 import { Product } from '../models/product';
 
 @Resolver()
@@ -7,8 +10,14 @@ export class ProductsResolver {
   constructor(private service: ProductsService) {}
 
   @Query(() => [Product])
-  // @UseGuards(AuthorizationGuard)
   products() {
     return this.service.listAllProducts();
+  }
+
+  @Mutation(() => Product)
+  @UseGuards(AuthorizationGuard)
+  createProduct(@Args('data') data: CreateProductInput) {
+    console.log('oakos');
+    return this.service.createProduct(data);
   }
 }
--- purchases/src/services/products.service.ts
+++ purchases/src/services/products.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
 import { PrismaService } from '../database/prisma/prisma.service';
+
+type CreateProductParams = {
+  title: string;
+};
 
 @Injectable()
 export class ProductsService {
@@ -7,5 +12,39 @@ export class ProductsService {
 
   async listAllProducts() {
     return await this.prisma.product.findMany();
+  }
+
+  async createProduct(data: CreateProductParams) {
+    const { title } = data;
+
+    // validando titulo
+    if (!title || title.trim().length === 0) {
+      throw new Error('Título é obrigatório!');
+    }
+
+    // gerando slug
+    const slug = slugify(title, {
+      lower: true,
+      trim: true,
+    });
+
+    if (await this.productWithSameSlug(slug)) {
+      throw new Error('Produto já cadastrado');
+    }
+
+    return await this.prisma.product.create({
+      data: {
+        slug,
+        title,
+      },
+    });
+  }
+
+  private async productWithSameSlug(slug: string) {
+    return !!(await this.prisma.product.findUnique({
+      where: {
+        slug,
+      },
+    }));
   }
 }
