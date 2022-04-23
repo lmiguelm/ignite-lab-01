14b4b7ab3757509a0bd51c460d40bf507ad54132
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2022-04-23 13:13:02 -0300

    feat(web): finalizando o front-end

--- web/src/pages/api/index.ts
+++ web/src/pages/api/index.ts
@@ -1,12 +1,12 @@
-import { getAccessToken } from '@auth0/nextjs-auth0';
+import { getAccessToken } from '@auth0/nextjs-auth0'
 import httpProxyMiddleware from 'next-http-proxy-middleware';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export const config = {
   api: {
-    bodyParser: false,
-  },
-};
+    bodyParser: false
+  }
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { accessToken } = await getAccessToken(req, res);
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   return httpProxyMiddleware(req, res, {
     target: 'http://localhost:3332/graphql',
     headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+      'Authorization': `Bearer ${accessToken}`
+    }
+  })
 }
